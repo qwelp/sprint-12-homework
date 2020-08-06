6abe6c342cd0435cfb41b51153af1b5e4574cdae
Vladimir Bun <qwelp@mail.ru>
2020-08-06 12:36:11 +0300

    Удалил лишние файлы

--- routes/cards.js
+++
@@ -1,8 +0,0 @@
-const cardsRouter = require('express').Router();
-const cardsItems = require('../data/cards.json');
-
-cardsRouter.get('/cards', (req, res) => {
-  res.send(cardsItems);
-});
-
-module.exports = cardsRouter;
